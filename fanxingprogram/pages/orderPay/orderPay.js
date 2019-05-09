df6927a34430fa5bad4a4a440720fbd8a543886e
Nick-Xv <2331945720@qq.com>
2019-05-09 17:39:53 +0800

    统一下单API
    本地测试成功

--- fanxingprogram/pages/orderPay/orderPay.js
+++ fanxingprogram/pages/orderPay/orderPay.js
@@ -1,5 +1,6 @@
 // fanxingprogram/pages/orderPay/orderPay.js
 const app = getApp();
+var MD5 = require('../../util/MD5.js');
 
 Page({
 
@@ -83,7 +84,22 @@ Page({
         var mchid = '1533779071';
         var detail = '商品详情';
         var notify_url = 'http://wxpay.wxutil.com/pub_v2/pay/notify.v2.php';
-        var tradeno = '123465';
+        var tradeno = '123';
+
+        var stringA;
+
+        stringA = "appid=" + appid + "&attach=" + attach + "&body=" + body + "&detail=" + detail + "&mch_id=" + mchid + "&nonce_str=" + ran.toString() + "&notify_url=" + notify_url + "&openid=" + app.globalData.user['_openid'] + "&out_trade_no=" + tradeno + "&spbill_create_ip=" + ip + "&total_fee=" + fee + "&trade_type=JSAPI";
+
+
+        var stringB;
+        stringB = stringA + "&key=2410c443d1eb69c6742c175a4c7927ab"
+        sign = MD5.md5(stringB).toUpperCase();
+
+        // this.setData({
+        //     ip: sign
+        // });
+
+        //["西红柿炒蛋","0.34","炒蛋",""]
 
         var dataString = '';
         dataString += '<xml><appid>';
@@ -108,11 +124,12 @@ Page({
         dataString += ip;
         dataString += '</spbill_create_ip><total_fee>';
         dataString += fee;
-        dataString += '</total_fee><trade_type>JSAPI</trade_type>';
+        dataString += '</total_fee><trade_type>JSAPI</trade_type><sign>';
         dataString += sign;
         dataString += '</sign></xml>'
 
 
+        console.log(dataString);
 
         this.setData({
             info: app.globalData.user['_openid']
@@ -123,12 +140,12 @@ Page({
                 data: dataString
             },
             complete: res => {
-                // this.setData({
-                //     info: res.result
-                // });
+                this.setData({
+                    ip: res.result
+                });
             }
         });
-        // "<xml>\r\n   <appid>wx2421b1c4370ec43b</appid>\r\n   <attach>支付测试</attach>\r\n   <body>JSAPI支付测试</body>\r\n   <mch_id>10000100</mch_id>\r\n   <detail><![CDATA[{ \"goods_detail\":[ { \"goods_id\":\"iphone6s_16G\", \"wxpay_goods_id\":\"1001\", \"goods_name\":\"iPhone6s 16G\", \"quantity\":1, \"price\":528800, \"goods_category\":\"123456\", \"body\":\"苹果手机\" }, { \"goods_id\":\"iphone6s_32G\", \"wxpay_goods_id\":\"1002\", \"goods_name\":\"iPhone6s 32G\", \"quantity\":1, \"price\":608800, \"goods_category\":\"123789\", \"body\":\"苹果手机\" } ] }]]></detail>\r\n   <nonce_str>1add1a30ac87aa2db72f57a2375d8fec</nonce_str>\r\n   <notify_url>http://wxpay.wxutil.com/pub_v2/pay/notify.v2.php</notify_url>\r\n   <openid>oUpF8uMuAJO_M2pxb1Q9zNjWeS6o</openid>\r\n   <out_trade_no>1415659990</out_trade_no>\r\n   <spbill_create_ip>14.23.150.211</spbill_create_ip>\r\n   <total_fee>1</total_fee>\r\n   <trade_type>JSAPI</trade_type>\r\n   <sign>0CB01533B8C1EF103065174F50BCA001</sign>\r\n</xml>"
+        // "<xml>\r\n   <appid>wx2421b1c4370ec43b</appid>\r\n   <attach>支付测试</attach>\r\n   <body>JSAPI支付测试</body>\r\n   <mch_id>10000100</mch_id>\r\n   <detail><![CDATA[{ \"goods_detail\":[ ] }]]></detail>\r\n   <nonce_str>1add1a30ac87aa2db72f57a2375d8fec</nonce_str>\r\n   <notify_url>http://wxpay.wxutil.com/pub_v2/pay/notify.v2.php</notify_url>\r\n   <openid>oUpF8uMuAJO_M2pxb1Q9zNjWeS6o</openid>\r\n   <out_trade_no>1415659990</out_trade_no>\r\n   <spbill_create_ip>14.23.150.211</spbill_create_ip>\r\n   <total_fee>1</total_fee>\r\n   <trade_type>JSAPI</trade_type>\r\n   <sign>0CB01533B8C1EF103065174F50BCA001</sign>\r\n</xml>"
         
     }
 })
